862384c22db006404402e1b760ab49eecd769884
Henokk-14 <54545858+Henokk-14@users.noreply.github.com>
2021-02-07 16:15:28 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+      int test = 10;
+      while(test >= 1){
+        System.out.println(test);
+        test--;
+      }
+    }
+}
