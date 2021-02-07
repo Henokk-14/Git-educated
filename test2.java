c87c6524004beb45447192c139cb4582d6e3f914
Henokk-14 <henokketsela14@gmail.com>
2021-02-07 16:58:47 -0500

    Added new file

---
+++ test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+    public static void main(String[] args) {
+      int test = 100;
+      while(test >= 1){
+        System.out.println(test);
+        test--;
+
+      }
+    }
+}
