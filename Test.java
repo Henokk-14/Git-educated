0dcba2538e03b9d0772b5e306e6cb80861c3c206
Henokk-14 <henokketsela14@gmail.com>
2021-02-07 17:11:50 -0500

    Added new variable to count prints

--- Test.java
+++ Test.java
@@ -2,9 +2,12 @@ public class Test {
   // counts down form 10
     public static void main(String[] args) {
       int test = 10;
+      int count = 0;
       while(test >= 1){
         System.out.println(test);
         test--;
+        count++;
       }
+      System.out.println("Amount of numbers printed is: " + count);
     }
 }
