d438f0636d78308def2618c52deef30216c2666d
Henokk-14 <henokketsela14@gmail.com>
2021-02-07 17:06:46 -0500

    Added comments and println

--- Test.java
+++ Test.java
@@ -1,10 +1,10 @@
 public class Test {
+  // counts down form 10
     public static void main(String[] args) {
       int test = 10;
       while(test >= 1){
         System.out.println(test);
         test--;
-//testing
       }
     }
 }
--- test2.java
+++ test2.java
@@ -1,10 +1,11 @@
 public class Test2 {
+    // counts down form 100
     public static void main(String[] args) {
       int test = 100;
       while(test >= 1){
         System.out.println(test);
-        test--;
-
+        test= test - 1;
       }
+        System.out.println("Done counting down from 100 and test = " + test);
     }
 }
