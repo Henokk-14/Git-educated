dfa3e6889ee8887024f2a7cfb63b273d835ad4d2
Henokk-14 <henokketsela14@gmail.com>
2021-02-23 12:28:09 -0500

    changed comments to include changes made to program

--- Test.java
+++ Test.java
@@ -1,11 +1,11 @@
 public class Test {
-  // counts down form 10
+   // counts up form 1
     public static void main(String[] args) {
       // variable to start counting from
       int test = 1;
       // variable for num of times counted
       int count = 0;
-      // loop to count down to 1
+      // loop to count up to 100
       while(test <= 100){
         System.out.println("test: " + test);
         test++;
