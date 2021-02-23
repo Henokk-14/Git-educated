056afd540fa69de3610893969ebbbb5470d54c2b
Henokk-14 <henokketsela14@gmail.com>
2021-02-23 12:26:11 -0500

    changed program to count up

--- Test.java
+++ Test.java
@@ -2,14 +2,13 @@ public class Test {
   // counts down form 10
     public static void main(String[] args) {
       // variable to start counting from
-      int test = 100;
+      int test = 1;
       // variable for num of times counted
       int count = 0;
       // loop to count down to 1
-      while(test >= 1){
+      while(test <= 100){
         System.out.println("test: " + test);
-        test--;
-        test--;
+        test++;
         count++;
       }
       // print string for num of times counted
